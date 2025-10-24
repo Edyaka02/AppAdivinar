1d1fd25add83656d8aa72647f2395702e1ef5be0
Edgar Manuel Carrillo Muruato <132968940+Edyaka02@users.noreply.github.com>
2025-10-23 18:08:48 -0700

    Add files via upload

--- AppAdivinaPalabra/AppAdivinaPalabra/ViewController.h
+++ AppAdivinaPalabra/AppAdivinaPalabra/ViewController.h
@@ -4,12 +4,24 @@
 //
 //  Created by Guest User on 23/10/25.
 //
-/*https://es.wallpapers.com/fondos-de-pantalla/logotipocon-halo-en-azul-neon-y-purpura-syt1zjoa0v3gheo4.html
-*/
+
  #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
 
+@property NSArray *palabras;
+@property NSArray *pistas;
+@property NSInteger indiceActual;
+@property NSInteger puntaje;
+@property BOOL iniciar;
+@property BOOL siguiente;
+
+@property (weak, nonatomic) IBOutlet UILabel *labelPuntaje;
+@property (weak, nonatomic) IBOutlet UILabel *labelMensaje;
+@property (weak, nonatomic) IBOutlet UITextField *campoRespuesta;
+@property (weak, nonatomic) IBOutlet UIButton *boton;
+
+- (IBAction)botonAccion:(id)sender;
 
 @end
 
--- AppAdivinaPalabra/AppAdivinaPalabra/ViewController.m
+++ AppAdivinaPalabra/AppAdivinaPalabra/ViewController.m
@@ -15,8 +15,88 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.palabras = @[@"cortana",
+                      @"warthog",
+                      @"aguijoneador",
+                      @"spartan",
+                      @"inquisidor",
+                      @"flood",
+                      @"reach",
+                      @"unsc",
+                      @"ghost",
+                      @"halo"];
+    
+    self.pistas = @[@"IA que acompaña al JefeMaestro.",
+                    @"Vehiculo todo terreno de la UNSC.",
+                    @"Arma con proyectiles de cristal rosado.",
+                    @"Soldado mejorado genéticamente.",
+                    @"Aliado Elite que se reveló contra los Profetas.",
+                    @"Enemigo parasitario.",
+                    @"Planeta militar clave de la humanidad.",
+                    @"Facción militar humana.",
+                    @"Vehiculo flotante del Covenant.",
+                    @"Estructura en forma de anillo."];
+    
+    self.indiceActual = -1;
+    self.puntaje = 0;
+    self.iniciar = NO;
+    self.siguiente = NO;
+    
+    self.campoRespuesta.hidden = YES;
+    self.labelPuntaje.hidden = YES;
 }
 
+- (IBAction)botonAccion:(id)sender {
+    if(!self.iniciar){
+        self.iniciar = YES;
+        self.indiceActual = 0;
+        self.campoRespuesta.hidden = NO;
+        self.labelPuntaje.hidden = NO;
+        [self mostrarPalabra];
+        [self.boton setTitle:@"Verificar" forState:UIControlStateNormal];
+    } else if (self.siguiente){
+        self.siguiente = NO;
+        
+        if(self.indiceActual < self.palabras.count){
+            [self mostrarPalabra];
+            [self.boton setTitle:@"Verificar" forState:UIControlStateNormal];
+            self.campoRespuesta.text = @"";
+            self.campoRespuesta.hidden = NO;
+        } else {
+            self.labelMensaje.text = @"Terminamos por hoy Jefe, buen trabajo.";
+            self.campoRespuesta.hidden = YES;
+            [self.boton setTitle:@"Comenzar" forState:UIControlStateNormal];
+            self.iniciar = NO;
+            self.indiceActual = -1;
+            self.puntaje = 0;
+            self.labelPuntaje.text = @"Puntos: 0";
+        }
+    } else {
+        [self verificarRespuesta];
+    }
+}
+
+- (void)mostrarPalabra {
+    self.labelMensaje.text = self.pistas[self.indiceActual];
+}
+
+- (void)verificarRespuesta{
+    NSString *respuesta = [self.campoRespuesta.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    NSString *correcto = self.palabras[self.indiceActual];
+    
+    if([respuesta caseInsensitiveCompare:correcto] == NSOrderedSame){
+        self.puntaje += 5;
+        self.labelMensaje.text = @"Bien Jefe, vamos por buen camino.";
+    } else {
+        self.labelMensaje.text = [NSString stringWithFormat:@"Incorrecto Jefe, me informan que era %@", correcto];
+    }
+    
+    self.labelPuntaje.text = [NSString stringWithFormat:@"Puntos: %d", (int)self.puntaje];
+    self.indiceActual++;
+    self.siguiente = YES;
+    //self.campoRespuesta.enabled = NO;
+    [self.boton setTitle:@"Siguiente" forState:UIControlStateNormal];
+}
 
 @end
